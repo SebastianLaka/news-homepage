7a7639082551ea14c18bedd2b695e3702e1facdd
Sebastian <sebastianlakla@gmail.com>
2026-02-04 18:40:56 +0100

    Add font file in scss, font family and used in reset

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,4 +2,10 @@
 <template>
     <h1>Hello :)</h1>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use '../assets/sass/fonts.scss' as *;
+h1{
+    font-size: $main-size;
+    font-weight: $ultra-bold-weight;
+}
+</style>
