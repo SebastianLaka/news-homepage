89cd3b62762c1ced23183da1ee722f19dd8ba380
Sebastian <sebastianlakla@gmail.com>
2026-02-07 21:22:27 +0100

    Added content to header slot

--- src/components/header/HeaderContent.vue
+++ src/components/header/HeaderContent.vue
@@ -1,23 +1,25 @@
 <script setup>
-import { ref } from 'vue';
-import MobileImageWeb from '../../assets/images/image-web-3-mobile.jpg';
-import DesktopImageWeb from '../../assets/images/image-web-3-desktop.jpg';
+import { ref } from 'vue'
+import MobileImageWeb from '../../assets/images/image-web-3-mobile.jpg'
+import DesktopImageWeb from '../../assets/images/image-web-3-desktop.jpg'
 const headerImage = ref({
-    mobileImage: MobileImageWeb,
-    desktopImage: DesktopImageWeb,
-    alt: 'Header news image'
+  mobileImage: MobileImageWeb,
+  desktopImage: DesktopImageWeb,
+  alt: 'Header news image',
 })
 </script>
 <template>
-    <header class="news-header">
-        <slot name="header-image" v-bind="headerImage"/>
-        <slot name="header-content"/>
-    </header>
+  <header class="news-header">
+    <slot name="header-image" v-bind="headerImage" />
+    <div class="news-header-content">
+      <slot name="header-content" />
+    </div>
+  </header>
 </template>
 <style lang="scss" scoped>
-@media (min-width: 375px){
-    .news-header{
-        padding-top: 4.5em;
-    }
+@media (min-width: 375px) {
+  .news-header {
+    padding-top: 4.5em;
+  }
 }
 </style>
--- src/components/header/HeaderMain.vue
+++ src/components/header/HeaderMain.vue
@@ -1,12 +1,19 @@
 <script setup>
-import HeaderContent from './HeaderContent.vue';
+import HeaderContent from './HeaderContent.vue'
 </script>
 <template>
-<HeaderContent>
-    <template #header-image="{mobileImage, alt}">
-        <img :src="mobileImage" :alt="alt">
+  <HeaderContent>
+    <template #header-image="{ mobileImage, alt }">
+      <img :src="mobileImage" :alt="alt" />
     </template>
-</HeaderContent>
+    <template #header-content>
+      <h1>The Bright Future of Web 3.0?</h1>
+      <p>
+        We dive into the next evolution of the web that claims to put the power of the platforms
+        back into the hands of the people. But is it really fulfilling its promise?
+      </p>
+      <button>read more</button>
+    </template>
+  </HeaderContent>
 </template>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
