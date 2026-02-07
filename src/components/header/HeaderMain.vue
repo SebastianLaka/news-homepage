6b0394310337fb07316101ce5fee37221fa38d61
Sebastian <sebastianlakla@gmail.com>
2026-02-07 21:57:11 +0100

    Added styles for header mobile

--- src/components/header/HeaderMain.vue
+++ src/components/header/HeaderMain.vue
@@ -8,22 +8,39 @@ import HeaderContent from './HeaderContent.vue'
     </template>
     <template #header-content>
       <div class="news-header-content">
-        <h1>The Bright Future of Web 3.0?</h1>
-        <p>
+        <h1 class="news-header-content__header">The Bright Future of Web 3.0?</h1>
+        <p class="news-header-content__description">
           We dive into the next evolution of the web that claims to put the power of the platforms
           back into the hands of the people. But is it really fulfilling its promise?
         </p>
-        <button>read more</button>
+        <button class="news-header-content__cta">read more</button>
       </div>
     </template>
   </HeaderContent>
 </template>
 <style lang="scss" scoped>
 @use '../../assets/sass/mixins.scss' as *;
+@use '../../assets//sass/fonts.scss' as *;
+@use '../../assets/sass/colors.scss' as *;
 @media (min-width: 375px) {
   .news-header-content {
-    @include flex-layout($flex-direction: column);
+    @include flex-layout($flex-direction: column, $align-items: start);
     gap: .75em 0;
+    padding-right: 1em;
+    &__header{
+        font-size: 3rem;
+    }
+    &__description{
+        max-width: 50ch;
+    }
+    &__cta{
+        padding: 1em 2em;
+        text-transform: uppercase;
+        letter-spacing: .15em;
+        font-weight: $bold-weight;
+        border: none;
+        background-color: getColor('soft-red');
+    }
   }
 }
 </style>
